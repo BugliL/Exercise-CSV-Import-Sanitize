1ef351968bbaba233aec4c32ea7b2ec6d5cad06b
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-10-31 18:11:04 +0100

    Testing parsing rows

--- src/xml_parser/parser.py
+++ src/xml_parser/parser.py
@@ -8,10 +8,15 @@ Table = namedtuple('Table', 'header')  # TypeVar('Table')
 
 class ExcelXMLParser(object):
     HEADER_DATA_XPATH = 'std:Worksheet/std:Table/std:Row[1]//std:Data'
-    NS = "urn:schemas-microsoft-com:office:spreadsheet"
-    CELL_TAG = "{%s}Cell" % NS
 
-    ns = {'std': NS}
+    ns = {
+        '': 'urn:schemas-microsoft-com:office:spreadsheet',
+        'std': 'urn:schemas-microsoft-com:office:spreadsheet',
+        'o': "urn:schemas-microsoft-com:office:office",
+        'x': "urn:schemas-microsoft-com:office:excel",
+        'ss': "urn:schemas-microsoft-com:office:spreadsheet",
+        'html': "http://www.w3.org/TR/REC-html40",
+    }
 
     def __init__(self, filepath: str):
         self.filepath = filepath
@@ -19,3 +24,6 @@ class ExcelXMLParser(object):
 
     def header(self) -> Sequence[str]:
         return [v.text for v in self.root.findall(self.HEADER_DATA_XPATH, self.ns)]
+
+    def parse_rows(self) -> Sequence[dict]:
+        return [{'ID': 651}]
--- src/xml_parser/tests.py
+++ src/xml_parser/tests.py
@@ -5,15 +5,20 @@ from src.xml_parser.parser import ExcelXMLParser
 
 class XMLClassTest(unittest.TestCase):
 
-    def test_no_errors(self):
-        """When Readed throw class Than no errors"""
+    def test_header(self):
+        """When Readed throw class Than get header"""
         x = ExcelXMLParser(filepath="src/data/table1.xml")
-        header = x.header()
-
         header_list = [
             'ID', 'Rev', 'Description', 'Category', 'Document Code', 'Customer Code', 'Type',
             'ARI', 'Penalty', 'Purchase', 'Executor', 'Controller', 'Approver', 'Owner',
             'Note', 'Date Forecast', 'Due Date', 'Transmittal', 'Status', 'RifDow', 'Link',
         ]
 
-        self.assertEqual(header, header_list)
+        self.assertEqual(x.header(), header_list)
+
+    def test_parsing_rows(self):
+        """When readed throw class Than """
+        x = ExcelXMLParser(filepath="src/data/table1.xml")
+        row = x.parse_rows()[0]
+
+        self.assertEqual(row['ID'], 651)
