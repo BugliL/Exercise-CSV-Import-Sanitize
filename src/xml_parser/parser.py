9dac99fbd9add2f69d4b2a77c10b7c38b8f7de9a
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-11-01 19:34:57 +0100

    Refactoring

--- src/xml_parser/parser.py
+++ src/xml_parser/parser.py
@@ -8,24 +8,70 @@ from xml.etree import ElementTree as ET, ElementTree
 Table = namedtuple('Table', 'header')  # TypeVar('Table')
 
 
-class ExcelXMLParser(object):
-    fields_to_ignore = ['Rev', 'Due Date', 'Transmittal', 'Status', ]
+class Cell(object):
+    CELL_DATA_XPATH = 'std:Data'
+    BASIC_NAMESPACE = 'urn:schemas-microsoft-com:office'
+    ATTRIBUTE_INDEX_XPATH = '{%s}Index' % (BASIC_NAMESPACE + ':spreadsheet')
+    ATTRIBUTE_TYPE_XPATH = '{%s}Type' % (BASIC_NAMESPACE + ':spreadsheet')
 
-    NS = 'urn:schemas-microsoft-com:office:spreadsheet'
-    ns = {
-        '': NS, 'std': NS, 'ss': NS,
-        'o': "urn:schemas-microsoft-com:office:office",
-        'x': "urn:schemas-microsoft-com:office:excel",
-        'html': "http://www.w3.org/TR/REC-html40",
+    FORMAT_DICT = {
+        'String': lambda x: str(x).strip(),
+        'Boolean': bool,
+        'Number': eval,
+        'DateTime': lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%f")
     }
 
-    ATTRIBUTE_INDEX_XPATH = '{%s}Index' % NS
-    ATTRIBUTE_TYPE_XPATH = '{%s}Type' % NS
+    def __init__(self, element: ElementTree, namespaces: dict, headers: Sequence[str]):
+        self.element = element
+        self.ns = namespaces
+        self.headers = headers
+
+    @property
+    def text(self):
+        return self.element.find(self.CELL_DATA_XPATH, self.ns).text
+
+    @property
+    def data(self):
+        return self.element.find(self.CELL_DATA_XPATH, self.ns)
+
+    @property
+    def value(self):
+        return self.get_function()(self.text)
+
+    @property
+    def has_index(self):
+        return self.ATTRIBUTE_INDEX_XPATH in self.element.attrib
+
+    @property
+    def type(self):
+        return self.data.attrib[self.ATTRIBUTE_TYPE_XPATH]
+
+    def _get_index(self):
+        return int(self.element.attrib[self.ATTRIBUTE_INDEX_XPATH])
+
+    def get_index(self, i: int):
+        return self._get_index() - 1 if self.has_index else i + 1
+
+    def get_function(self):
+        return self.FORMAT_DICT.get(self.type, str)
+
+
+class ExcelXMLParser(object):
+    fields_to_ignore = ['Rev', 'Due Date', 'Transmittal', 'Status', ]
+    BASIC_NAMESPACE = 'urn:schemas-microsoft-com:office'
+
+    ns = {
+        '': BASIC_NAMESPACE + ':spreadsheet',
+        'std': BASIC_NAMESPACE + ':spreadsheet',
+        'ss': BASIC_NAMESPACE + ':spreadsheet',
+        'o': BASIC_NAMESPACE + ":office",
+        'x': BASIC_NAMESPACE + ":excel",
+        'html': "http://www.w3.org/TR/REC-html40",
+    }
 
     HEADER_DATA_XPATH = 'std:Worksheet/std:Table/std:Row[1]//std:Data'
     DATA_ROWS_XPATH = 'std:Worksheet/std:Table/std:Row'
     CELL_XPATH = 'std:Cell'
-    CELL_DATA_XPATH = 'std:Data'
 
     def __init__(self, filepath: str):
         self.filepath = filepath
@@ -36,29 +82,17 @@ class ExcelXMLParser(object):
 
     def parse_rows(self) -> Sequence[dict]:
 
-        select_index = lambda c, i: \
-            int(c.attrib[self.ATTRIBUTE_INDEX_XPATH]) - 1 \
-                if self.ATTRIBUTE_INDEX_XPATH in c.attrib else i + 1
-
-        fn = {
-            'String': lambda x: str(x).strip(),
-            'Boolean': bool,
-            'Number': eval,
-            'DateTime': lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%f")
-        }
-
         header = self.header()
         lst = []
         for row in self.root.findall(self.DATA_ROWS_XPATH, self.ns)[1:]:
             i = -1
             data = {k: None for k in header if k not in self.fields_to_ignore}
             for c in row.findall(self.CELL_XPATH, self.ns):
-                i = select_index(c, i)
-                d = c.find(self.CELL_DATA_XPATH, self.ns)
+                cell = Cell(element=c, namespaces=self.ns, headers=header)
+                i = cell.get_index(i)
                 k = header[i]
-                if d is not None and k not in self.fields_to_ignore:
-                    format = fn.get(d.attrib[self.ATTRIBUTE_TYPE_XPATH], str)
-                    data[k] = format(d.text)
+                if cell.data is not None and k not in self.fields_to_ignore:
+                    data[k] = cell.value
 
             lst.append(data)
 
--- src/xml_parser/tests.py
+++ src/xml_parser/tests.py
@@ -7,31 +7,32 @@ from src.xml_parser.parser import ExcelXMLParser
 
 class XMLClassTest(unittest.TestCase):
 
+    def setUp(self):
+        self.parser = ExcelXMLParser(filepath="src/data/table1.xml")
+
     def test_header(self):
         """When Readed throw class Than get header"""
-        x = ExcelXMLParser(filepath="src/data/table1.xml")
         header_list = [
             'ID', 'Rev', 'Description', 'Category', 'Document Code', 'Customer Code', 'Type',
             'ARI', 'Penalty', 'Purchase', 'Executor', 'Controller', 'Approver', 'Owner',
             'Note', 'Date Forecast', 'Due Date', 'Transmittal', 'Status', 'RifDow', 'Link',
         ]
 
-        self.assertEqual(x.header(), header_list)
+        self.assertEqual(self.parser.header(), header_list)
 
     def test_parsing_rows(self):
-        """When readed throw class Than """
-        x = ExcelXMLParser(filepath="src/data/table1.xml")
-        row = x.parse_rows()[0]
+        """When readed throw class Than parse rows"""
+        row = self.parser.parse_rows()[0]
 
         self.assertEqual(651, row['ID'])
         self.assertEqual('GENERAL ARRANGEMENT DRAWING', row['Description'])
         self.assertEqual('PLANT', row['Category'])
 
-        row = x.parse_rows()[1]
+        row = self.parser.parse_rows()[1]
         self.assertEqual(None, row['Note'])
 
-        row = x.parse_rows()[2]
+        row = self.parser.parse_rows()[2]
         self.assertEqual('Prova', row['Note'])
 
-        row = x.parse_rows()[-1]
+        row = self.parser.parse_rows()[-1]
         self.assertEqual('', row['Link'])
