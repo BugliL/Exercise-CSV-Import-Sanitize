15ad1d2354c1e86c77be3b5a51526a1966a21b52
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-11-02 19:39:18 +0100

    Refactoring

--- src/xml_parser/parser.py
+++ src/xml_parser/parser.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 from collections import namedtuple
 from functools import reduce
-from typing import Sequence
+from typing import Sequence, List
 
 from xml.etree import ElementTree as ET, ElementTree
 
-Table = namedtuple('Table', 'header')  # TypeVar('Table')
+
+# Table = namedtuple('Table', 'header')  # TypeVar('Table')
 
 
-class Namespaces(object):
+class Contants(object):
     fields_to_ignore = ['Rev', 'Due Date', 'Transmittal', 'Status', ]
     BASIC_NAMESPACE = 'urn:schemas-microsoft-com:office'
 
-    ALL = {
+    ALL_NS = {
         '': BASIC_NAMESPACE + ':spreadsheet',
         'std': BASIC_NAMESPACE + ':spreadsheet',
         'ss': BASIC_NAMESPACE + ':spreadsheet',
@@ -38,18 +39,17 @@ class Cell(object):
         'DateTime': lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%f")
     }
 
-    def __init__(self, element: ElementTree, namespaces: dict, headers: Sequence[str]):
+    def __init__(self, element: ElementTree, headers: Sequence[str]):
         self.element = element
-        self.ns = namespaces
         self.headers = headers
 
     @property
     def text(self):
-        return self.element.find(Namespaces.CELL_DATA_XPATH, Namespaces.ALL).text
+        return self.element.find(Contants.CELL_DATA_XPATH, Contants.ALL_NS).text
 
     @property
     def data(self):
-        return self.element.find(Namespaces.CELL_DATA_XPATH, Namespaces.ALL)
+        return self.element.find(Contants.CELL_DATA_XPATH, Contants.ALL_NS)
 
     @property
     def value(self):
@@ -57,20 +57,40 @@ class Cell(object):
 
     @property
     def has_index(self):
-        return Namespaces.ATTRIBUTE_INDEX_XPATH in self.element.attrib
+        return Contants.ATTRIBUTE_INDEX_XPATH in self.element.attrib
 
     @property
     def type(self):
-        return self.data.attrib[Namespaces.ATTRIBUTE_TYPE_XPATH]
+        return self.data.attrib[Contants.ATTRIBUTE_TYPE_XPATH]
 
     def _get_index(self):
-        return int(self.element.attrib[Namespaces.ATTRIBUTE_INDEX_XPATH])
+        return int(self.element.attrib[Contants.ATTRIBUTE_INDEX_XPATH])
+
+    @property
+    def cell_index(self):
+        return self._get_index()
 
     def get_index(self, i: int):
         return self._get_index() - 1 if self.has_index else i + 1
 
     def get_function(self):
         return self.FORMAT_DICT.get(self.type, str)
+
+
+class Table(object):
+    def __init__(self, table_element: ElementTree):
+        self.root = table_element
+        self._header(table_element)
+        self._body(table_element)
+
+    def _header(self, table_element: ElementTree):
+        return [
+            v.text for v in \
+            table_element.findall(Contants.HEADER_DATA_XPATH, Contants.ALL_NS)
+        ]
+
+    def _body(self, table_element: ElementTree):
+        raise NotImplementedError()
 
 
 class ExcelXMLParser(object):
@@ -80,17 +100,17 @@ class ExcelXMLParser(object):
         self.root = ET.parse(filepath).getroot()  # type: ET
 
     def header(self) -> Sequence[str]:
-        return [v.text for v in self.root.findall(Namespaces.HEADER_DATA_XPATH, Namespaces.ALL)]
+        return [v.text for v in self.root.findall(Contants.HEADER_DATA_XPATH, Contants.ALL_NS)]
 
     def parse_rows(self) -> Sequence[dict]:
         header = self.header()
         lst = []
-        for row_element in self.root.findall(Namespaces.DATA_ROWS_XPATH, Namespaces.ALL)[1:]:
+        for row_element in self.root.findall(Contants.DATA_ROWS_XPATH, Contants.ALL_NS)[1:]:
             row = row_element
             i = -1
             data = {k: None for k in header}
-            for c_element in row.findall(Namespaces.CELL_XPATH, Namespaces.ALL):
-                cell = Cell(element=c_element, namespaces=Namespaces.ALL, headers=header)
+            for c_element in row.findall(Contants.CELL_XPATH, Contants.ALL_NS):
+                cell = Cell(element=c_element, headers=header)
                 i = cell.get_index(i)
                 k = header[i]
                 if cell.data is not None:
