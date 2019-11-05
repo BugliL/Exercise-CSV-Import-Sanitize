cd0e1dba765633dff66e7b9506ae59003fb0314a
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-11-05 16:48:58 +0100

    Bugfixed test class

--- src/xml_parser/tests.py
+++ src/xml_parser/tests.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 from dataclasses import dataclass
 
@@ -6,26 +7,31 @@ from src.xml_parser.parser import ExcelXMLParser, Row
 
 @dataclass
 class DjangoModel(object):
-    pk = None
-    description = None
-    category = None
-    document_code = None
-    customer_code = None
-    type = None
-    arip = None
-    penalty = None
-    purchase = None
-    executor = None
-    controller = None
-    approver = None
-    owner = None
-    note = None
-    date_forecast = None
-    link = None
-    rifdow = None
+    pk: int = None
+    description: str = None
+    category: str = None
+    document_code: str = None
+    customer_code: str = None
+    type: str = None
+    arip: str = None
+    penalty: bool = None
+    purchase: bool = None
+    executor: str = None
+    controller: str = None
+    approver: str = None
+    owner: str = None
+    note: str = None
+    date_forecast: datetime.datetime = None
+    link: str = None
+    rifdow: str = None
+    project: int = None
 
 
 class XMLClassTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.maxDiff = None
 
     def setUp(self):
         self.parser = ExcelXMLParser(filepath="src/data/table1.xml")
@@ -45,7 +51,7 @@ class XMLClassTest(unittest.TestCase):
         """When readed throw class Than parse rows"""
         row = self.parser.parse_rows()[0]
 
-        self.assertEqual(651, row['id'])
+        self.assertEqual(None, row['id'])
         self.assertEqual('GENERAL ARRANGEMENT DRAWING', row['description'])
         self.assertEqual('PLANT', row['category'])
         self.assertEqual('CRD700220', row['document_code'])
@@ -73,3 +79,34 @@ class XMLClassTest(unittest.TestCase):
         self.assertRaises(AttributeError, getattr, model, 'rev')
         self.assertRaises(AttributeError, getattr, model, 'due_date')
         self.assertRaises(AttributeError, getattr, model, 'status')
+
+    def test_functional(self):
+        """Testing functional programming"""
+        EXCLUDE_PARAM_LIST = ['id', 'rev', 'status', 'transmittal', 'due_date', ]
+
+        model0 = DjangoModel()
+        model0.project = 12
+        row0 = self.parser.objects(EXCLUDE=EXCLUDE_PARAM_LIST)[0]  # type: Row
+        row0.fill(model0)
+
+        model1 = DjangoModel()
+        row1 = self.parser.objects(EXCLUDE=EXCLUDE_PARAM_LIST)[1]  # type: Row
+        row1.fill(model1)
+        model1.project = 12
+        dummy_list = [model0, model1]
+
+        set_project = lambda x: setattr(x, 'project', 12) or x
+        get_model = lambda row: DjangoModel()
+        fill_model = lambda row, model: row.fill(model)
+
+        record_list = [
+            fill_model(row, set_project(get_model(row)))
+            for row in self.parser.objects(EXCLUDE=EXCLUDE_PARAM_LIST)
+        ]
+
+        self.assertListEqual(dummy_list, record_list[:2])
+        self.assertEqual(dummy_list[0].description, record_list[0].description)
+        self.assertEqual(dummy_list[0].document_code, record_list[0].document_code)
+        self.assertEqual('CRD700220', dummy_list[0].document_code)
+        self.assertEqual(12, dummy_list[0].project)
+        self.assertNotEqual('ayeye', dummy_list[0].document_code)
